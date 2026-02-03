67004c529ddbc435dbf3a57cc93ec6d465abcfa0
Akshay Garg <akshay@propelgtm.com>
2026-02-04 02:00:28 +0530

    pr comments

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ import { Login } from "./pages/login";
 import { Register } from "./pages/register";
 
 function App() {
+  console.log("App rendered");
   return (
     <BrowserRouter>
       <GitHubBanner />
